d63d0dcb666f6b73dd78171a3b0e979085c12514
Young Ha Yoo <yyoo@princeton.edu>
2022-11-29 15:51:23 -0500

    Add function to clean up obstacles in obstacle manager

--- src/components/managers/ObstacleManager/ObstacleManager.js
+++ src/components/managers/ObstacleManager/ObstacleManager.js
@@ -3,10 +3,29 @@ import { Bird, Cactus } from 'objects';
 class ObstacleManager {
     constructor(obstacleParent) {
         this.obstacles = []; // array to hold obstacles
+        this.inactiveObstacleSet = new Set(); // set to hold inactive obstacles
 
         // generate 2 obstacles for now
         this.obstacles.push(new Cactus(obstacleParent));
         this.obstacles.push(new Bird(obstacleParent));
+    }
+
+    // clean up obstacles behind given frontier depth
+    cleanUpObstacles(frontierDepth) {
+        // clean up active obstacles if they are behind frontier depth
+        for (let i = 0; i < this.obstacles.length; i++) {
+            const obstacle = this.obstacles[i]; 
+            // do not check if obstacle is inactive
+            if (this.inactiveObstacleSet.has(obstacle)) {
+                continue;
+            }
+            else {
+                if (obstacle.position.z < frontierDepth) {
+                    obstacle.garbageCollect();
+                    this.inactiveObstacleSet.add(obstacle);
+                }
+            }
+        }
     }
 }
 
