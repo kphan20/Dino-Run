f3d0088120a022e297c36004ab569f64605cce56
Young Ha Yoo <yyoo@princeton.edu>
2022-12-06 20:48:12 -0500

    Implement frustum culling

--- src/app.js
+++ src/app.js
@@ -10,8 +10,10 @@ import {
     WebGLRenderer,
     PerspectiveCamera,
     Vector3,
+    Vector4,
     RGB_S3TC_DXT1_Format,
 } from 'three';
+
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
 import * as CANNON from 'cannon-es';
@@ -99,6 +101,68 @@ const handleCollisions = () => {
     }
 };
 
+// Frustum culling
+const handleFrustumCulling = () => {
+    scene.traverse((obj) => {
+        obj.visible = inFrustum(obj);
+    })
+}
+const inFrustum = (obj) => {    
+    const projectionMatrix = camera.projectionMatrix; 
+    const matrix = projectionMatrix;
+    let boundingBox = null;
+    if (obj == undefined || obj.geometry == undefined || obj.geometry.boundingBox == null) {
+        return true;
+    }
+    if (obj.geometry != undefined) {
+        obj.geometry.computeBoundingBox();
+        boundingBox = obj.geometry.boundingBox; 
+    } else {
+        obj.children[0].geometry.computeBoundingBox();
+        boundingBox = obj.children[0].geometry.boundingBox;
+    }
+
+    // 8 vertices
+    const min = boundingBox.min; 
+    const max = boundingBox.max;
+
+    const v1 = new Vector3(min.x, min.y, min.z);
+    const v2 = new Vector3(min.x, min.y, max.z);
+    const v3 = new Vector3(min.x, max.y, min.z);
+    const v4 = new Vector3(min.x, max.y, max.z);
+    const v5 = new Vector3(max.x, min.y, min.z);
+    const v6 = new Vector3(max.x, min.y, max.z);
+    const v7 = new Vector3(max.x, max.y, min.z);
+    const v8 = new Vector3(max.x, max.y, max.z);
+    const verts = [
+        v1, v2, v3, v4, v5, v6, v7, v8,
+    ];
+
+    for (let i = 0; i < verts.length; i++) {
+        const v = verts[i];
+        const worldV = obj.localToWorld(v);
+        const worldV4 = new Vector4(worldV.x, worldV.y, worldV.z, 1);
+        const cameraV4 = worldV4.applyMatrix4(camera.matrixWorldInverse);
+
+        const projV4 = cameraV4.applyMatrix4(matrix);
+        const x = projV4.x; 
+        const y = projV4.y; 
+        const z = projV4.z; 
+        const w = projV4.w; 
+
+        if (
+            x >= -w && 
+            x <= w &&
+            y >= -w &&
+            y <= w &&
+            z >= 0 &&
+            z <= w
+        ) return true; 
+    }
+
+    return false;
+}
+
 // cannon debugger
 // const cannonDebugger = new CannonDebugger(scene, physicsWorld);
 
@@ -126,6 +190,9 @@ const onAnimationFrameHandler = (timeStamp) => {
         scene.obstacleManager.handleObstacles(scene.player.position.z);
     }
     window.requestAnimationFrame(onAnimationFrameHandler);
+
+    scene.obstacleManager.handleObstacles(scene.player.position.z);
+    handleFrustumCulling();
 };
 window.requestAnimationFrame(onAnimationFrameHandler);
 
