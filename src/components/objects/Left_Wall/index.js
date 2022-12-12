c920fda9052efef0d9baf2dc0c2a7f8585949670
anthonyng39 <anthonyng324@gmail.com>
2022-12-12 14:48:10 -0500

    added left wall

---
+++ src/components/objects/Left_Wall/index.js
@@ -0,0 +1,1 @@
+export { default as Left_Wall } from './Left_Wall.js';
--- src/components/objects/index.js
+++ src/components/objects/index.js
@@ -3,3 +3,4 @@ export * from './Cactus';
 export * from './Flower';
 export * from './Land';
 export * from './Floor';
+export * from './Left_Wall';
